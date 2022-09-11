ad976a10ed46d31cd9efa496f0d24592e4031fb8
N0TARNI3 <mail.arniefraga@gmail.com>
2022-09-11 21:31:29 +0800

    Add container styles (09/11/22)

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Input from './components/tokens/Input';
 
 function App() {
   return (
-    <div>
-      <H1 text="Welcome to"/>
-      <H4 text="Electronic Health Record (EHR) De-identification Tool"/>
-      <Button title="Export Redacted Document" type="btn-primary"/>      
-      <Button title="Doctor" type="btn-secondary"/>     
-      <Label text="email" type="lbl-solid"/> 
-      <Label text="Don’t have an account yet? Sign up" type="lbl-light"/> 
-      <Input type="email" placeholder="johndoe@mail.com"/>
-      <Input type="password" placeholder="6 characters required"/>
-      <Input type="text" placeholder="John Doe"/>
-      <p> Ms. Louise Iles NAME-PATIENT is a 70 AGE year old female patient of Dr. Xue NAME-DOCTOR here today with first visit with me for several issues. Met with PCP in Feb DATE for multiple issues, DM changed from Actos to  Metformin, referral to Internal Medicine; GERD/H-pylori,  (on meds, GI consult) HTN (changed from nitrates to Lisinopril) cholesterol, HA (lab done to r/o TA and PMR) Hepatitis.</p>
+    <div className='content'>
+      <div className='container'>
+        <H1 text="Welcome to"/>
+        <H4 text="Electronic Health Record (EHR) De-identification Tool"/>
+        <Button title="Export Redacted Document" type="btn-primary"/>      
+        <Button title="Doctor" type="btn-secondary"/>     
+        <Label text="email" type="lbl-solid"/> 
+        <Label text="Don’t have an account yet? Sign up" type="lbl-light"/> 
+        <Input type="email" placeholder="johndoe@mail.com"/>
+        <Input type="password" placeholder="6 characters required"/>
+        <Input type="text" placeholder="John Doe"/>
+        <p> Ms. Louise Iles NAME-PATIENT is a 70 AGE year old female patient of Dr. Xue NAME-DOCTOR here today with first visit with me for several issues. Met with PCP in Feb DATE for multiple issues, DM changed from Actos to  Metformin, referral to Internal Medicine; GERD/H-pylori,  (on meds, GI consult) HTN (changed from nitrates to Lisinopril) cholesterol, HA (lab done to r/o TA and PMR) Hepatitis.</p>
+      </div>
     </div>
   );
 }
